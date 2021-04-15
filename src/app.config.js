5882d03c51c92fac8f18d56b9b0886a8bb54d26a
Yash Bajaj <yasbajaj@deloitte.com>
2021-04-15 15:53:13 +0000

    Initial commit

---
+++ src/app.config.js
@@ -0,0 +1,11 @@
+/**
+ * Place to store data specific to this instance
+ * DO NOT store secrets here
+ * DO NOT store state here - must be part of a stateless service
+ */
+class AppConfig {
+    version = '1.0.1';
+    team = 'Green Dot Cloud';
+}
+module.exports = AppConfig;
+
